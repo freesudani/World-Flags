96559c684ac46d31d9c199f39e9fa00a660e9f8a
Edward Baldacchino <edward.baldacchino@live.vu.edu.au>
2021-09-04 15:45:36 +1000

    Returned button in optionslist[0].options.map function. Centred modal using flex

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,54 +1,33 @@
-import React from "react";
-import "./Instructions.css";
-import "./button.css";
-import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
-
-const InstructionsVariant = {
-  hidden: {
-    opacity: 0,
-    x: "-100vw",
-    y: "-10vw",
-  },
-  visible: {
-    opacity: 1,
-    x: "-22vw",
-    y: "-10vw",
-    transition: {
-      type: "spring",
-      delay: 0.5,
-    },
-  },
-  exit: {
-    x: "-100vw",
-    transition: { ease: "easeInOut" },
-  },
-};
+import React from 'react';
+import './Instructions.css';
+import './button.css';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import transitionVariants from './transitionVariants';
 
 export const Instructions = () => {
-  return (
-    <motion.div
-      className="instructions-box"
-      variants={InstructionsVariant}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-    >
-      <h1>Please Read Below Instructions</h1>
-      <div>
-        <p>1. You will presented with 5 different flags</p>
-        <p>2. Please choose the correct flag out of the options</p>
-        <p>3. You can Exit the Quize at any stage</p>
-      </div>
-      <div className="button-container">
-        <Link to="/Question1">
-          <button className="btn">Proceed</button>
-        </Link>
+	return (
+		<motion.div
+			className='instructions-box modal'
+			variants={transitionVariants}
+			initial='hidden'
+			animate='visible'
+			exit='hidden'>
+			<h1>Please Read Below Instructions</h1>
+			<div>
+				<p>1. You will presented with 5 different flags</p>
+				<p>2. Please choose the correct flag out of the options</p>
+				<p>3. You can Exit the Quize at any stage</p>
+			</div>
+			<div className='button-container'>
+				<Link to='/Question1'>
+					<button className='btn'>Proceed</button>
+				</Link>
 
-        <Link to="/">
-          <button className="btn">Exit</button>
-        </Link>
-      </div>
-    </motion.div>
-  );
+				<Link to='/'>
+					<button className='btn'>Exit</button>
+				</Link>
+			</div>
+		</motion.div>
+	);
 };
--- src/components/Question1.js
+++ src/components/Question1.js
@@ -1,66 +1,68 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-import { QuestionsList } from "../data/Q1data";
-import "./Question.css";
-import "./button.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { QuestionsList } from '../data/Q1data';
+import './Question.css';
+import './button.css';
+import transitionVariants from './transitionVariants';
 
 const Question1Variant = {
-  hidden: {
-    opacity: 0,
-    x: "-100vw",
-    y: "-17vw",
-  },
-  visible: {
-    opacity: 1,
-    x: "-29vw",
-    y: "-17vw",
-    transition: {
-      type: "spring",
-      delay: 0.5,
-    },
-  },
-  exit: {
-    x: "-100vw",
-    transition: { ease: "easeInOut" },
-  },
+	hidden: {
+		opacity: 0,
+		x: '-100vw',
+		y: '-17vw',
+	},
+	visible: {
+		opacity: 1,
+		x: '-29vw',
+		y: '-17vw',
+		transition: {
+			type: 'spring',
+			delay: 0.5,
+		},
+	},
+	exit: {
+		x: '-100vw',
+		transition: { ease: 'easeInOut' },
+	},
 };
 
 export const Question1 = () => {
-  const [answer, setAnswer] = useState(false);
-  const [wrong, setWrong] = useState(false);
+	const [answer, setAnswer] = useState(false);
+	const [wrong, setWrong] = useState(false);
 
-  const ChoosenOption = (answer || wrong) && !(answer && wrong);
+	const ChoosenOption = (answer || wrong) && !(answer && wrong);
 
-  return (
-    <motion.div
-      className="question-box"
-      variants={Question1Variant}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-    >
-      <div className="question-flag">
-        <img src={QuestionsList[0].image} alt="photo" />
-      </div>
-      <div className="question-heading">
-        <h1>Pick The Right Country's Flag</h1>
-      </div>
-      <div className="question-option">
-        {QuestionsList[0].options.map((option, index) => {
-          <button key={index} className="question-option-paragraph">
-            {option.Answertext}
-          </button>;
-        })}
-      </div>
-      <div className="button-container">
-        <Link to="">
-          <button className="btn">Proceed</button>
-        </Link>
-        <Link to="/">
-          <button className="btn">Exit</button>
-        </Link>
-      </div>
-    </motion.div>
-  );
+	return (
+		<motion.div
+			className='question-box modal'
+			variants={transitionVariants}
+			initial='hidden'
+			animate='visible'
+			exit='hidden'>
+			<div className='question-flag'>
+				<img src={QuestionsList[0].image} alt='photo' />
+			</div>
+			<div className='question-heading'>
+				<h1>Pick The Right Country's Flag</h1>
+			</div>
+			<div className='question-option'>
+				{QuestionsList[0].options.map((option, index) => {
+					return (
+						<button key={index} className='question-option-paragraph'>
+							{option.Answertext}
+						</button>
+					);
+				})}
+			</div>
+			<div className='button-container'>
+				<Link to=''>
+					<button className='btn'>Proceed</button>
+				</Link>
+				<Link to='/'>
+					<button className='btn'>Exit</button>
+				</Link>
+			</div>
+		</motion.div>
+	);
 };
